99c742bb9081caa96e3b54f2273f4e46c54d9ba1
MattBrozusky <matthew.brozusky@gmail.com>
2018-12-12 13:52:59 -0600

    finished exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -7,17 +7,27 @@ import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
+
+    static boolean validAttempt;
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+
+        if (validAttempt){
+            response.sendRedirect("/profile");
+        } else {
+            request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        }
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        boolean validAttempt = username.equals("admin") && password.equals("password");
+        validAttempt = (username.equals("admin") || username.equals("user")) && password.equals("password");
 
         if (validAttempt) {
+            request.getSession().setAttribute("validAttempt", true);
             response.sendRedirect("/profile");
+            request.setAttribute("userStatus", username);
         } else {
             response.sendRedirect("/login");
         }
